ce8d674c3e17c47b80d47f42fe6bec9939c0b37e
chasedashiell <dashchax@gmail.com>
2023-03-31 11:57:29 -0500

    finished

--- ArrayListUtilities.java
+++ ArrayListUtilities.java
@@ -1,7 +1,53 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListUtilities {
     public static int getListMax(ArrayList<Integer> items) {
-        return 0;
+        int max = items.get(0);
+        for(int index = 0; index < items.size(); index++){
+            if( items.get(index) > max){
+                max = items.get(index);
+            }
+        }
+        return max;
     }
+
+    public static ArrayList<String> withoutLongWords(ArrayList<String> items){
+        ArrayList<String> solution = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++){
+            if (items.get(i).length() < 6){
+                solution.add(items.get(i));
+            }
+        }
+        return solution;
+    }
+
+    public static void removeLongWords(ArrayList<String> items){
+        for (int i = 0; i < items.size(); i++){
+            if (items.get(i).length() > 5){
+                items.remove(items.get(i));
+            }
+        }
+    }
+
+    public static ArrayList<Double> reversed(ArrayList<Double> items){
+        ArrayList<Double> solution = new ArrayList<>();
+        for(int i = items.size() - 1; i >= 0; i--){
+            solution.add(items.get(i));
+        }
+        return solution;
+    }
+
+    public static void reverseInPlace(ArrayList<Double> items){
+        int countDown = items.size()-1;
+        for (int i = 0; i < items.size() / 2; i++){
+            Double temp = items.get(i);
+            items.set(i, items.get(countDown));
+            items.set(countDown, temp);
+            System.out.println(items);
+            countDown--;
+        }
+    }
+
 }
--- ArrayListUtilitiesTest.java
+++ ArrayListUtilitiesTest.java
@@ -17,5 +17,54 @@ public class ArrayListUtilitiesTest {
         expectedOutput = 9;
         actualOutput = ArrayListUtilities.getListMax(input);
         assertEquals(expectedOutput, actualOutput);
+
+        
     }
+    @Test
+    public void testWithoutLongWords(){
+            ArrayList<String> input;
+            ArrayList<String> expectedOutput;
+            ArrayList<String> actualOutput;
+
+            input = new ArrayList<>(Arrays.asList("hello", "my", "name", "is", "theGingerBreadMan"));
+            expectedOutput = new ArrayList<>(Arrays.asList("hello", "my", "name", "is"));
+            actualOutput = ArrayListUtilities.withoutLongWords(input);
+            assertEquals(expectedOutput, actualOutput);
+
+    }
+    @Test
+    public void testRemoveLongWords(){
+        ArrayList<String> input;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+        input = new ArrayList<>(Arrays.asList("hello", "my", "name", "is", "theGingerBreadMan"));
+        expectedOutput = new ArrayList<>(Arrays.asList("hello", "my", "name", "is"));
+        ArrayListUtilities.removeLongWords(input);
+        actualOutput = input;
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test 
+    public void testReversed(){
+        ArrayList<Double> input;
+        ArrayList<Double> expectedOutput;
+        ArrayList<Double> actualOutput;
+
+        input = new ArrayList<>(Arrays.asList(1.0, 2.6, 1.3, 6.4, 0.6));
+        expectedOutput = new ArrayList<>(Arrays.asList(0.6, 6.4, 1.3, 2.6, 1.0));
+        actualOutput = ArrayListUtilities.reversed(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testReverseInPlace(){
+        ArrayList<Double> input;
+        ArrayList<Double> expectedOutput;
+        input = new ArrayList<>(Arrays.asList(1.0, 2.6, 1.3, 6.4, 0.6));
+        expectedOutput = new ArrayList<>(Arrays.asList(0.6, 6.4, 1.3, 2.6, 1.0));
+        ArrayListUtilities.reverseInPlace(input);
+        assertEquals(expectedOutput, input);
+    }
+
 }
